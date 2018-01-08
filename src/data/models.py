42fcff2d328ec281e1a07846c0554a680c6e495c
Briet Sparks <bsparks@dal0-03265.na.corp.jwt.com>
2018-01-08 14:55:21 -0600

    starting new schema

--- src/data/models.py
+++ src/data/models.py
@@ -1,23 +1,37 @@
 from app import db
 
+ability_contributions = db.Table(
+    'ability_contribution',
+    db.Column('ability_id', db.Integer, db.ForeignKey('ability.id')),
+    db.Column('contribution_id', db.Integer, db.ForeignKey('contribution.id'))
+)
 
-project_skills = db.Table(
-    'project_skill',
-    db.Column('project_id', db.Integer, db.ForeignKey('project.id')),
+contribution_skills = db.Table(
+    'contribution_skill',
+    db.Column('contribution_id', db.Integer, db.ForeignKey('contribution.id')),
     db.Column('skill_id', db.Integer, db.ForeignKey('skill.id'))
 )
 
 
-class Project(db.Model):
+class Ability(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.String())
+    short_summary = db.Column(db.String())
+
+
+class Contribution(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     owner_id = db.Column(db.String())
     title = db.Column(db.String())
     subtitle = db.Column(db.String())
-    skills = db.relationship('Skill', secondary=project_skills, backref='projects')
+    skills = db.relationship('Skill', secondary=contribution_skills, backref='contributions')
 
+
+# class Employer(db.Model):
+#     id =
 
 class Skill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     creator_id = db.Column(db.String())
     name = db.Column(db.String())
-    # projects = db.relationship('Project', secondary=project_skills, backref='skills')
+    # contributions = db.relationship('Contribution', secondary=contribution_skills, backref='skills')
